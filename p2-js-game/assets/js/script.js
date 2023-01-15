0b29b5542c10b8529644d545d57b6886fea8332c
aramind <r.m.miranda.ece@gmail.com>
2023-01-15 12:52:41 +0800

    feat: major changes in the codes
      - rewriting the js code in a new js file
      - replacing some code implementation and logic with better implementations
      - adding the preplay options panel

--- p2-js-game/assets/js/script.js
+++ p2-js-game/assets/js/script.js
@@ -1,4 +1,6 @@
-// GLOBAL VARIABLES
+/*================================================================
+|                 GLOBAL GAME VARIABLE                            |
+/================================================================*/
 
 let difficulty = 3;
 let correctAnswer;
@@ -6,9 +8,12 @@ let score = 0;
 let cycleTime = 7000;
 let hasClicked = false;
 let cycle;
+let popOfHumans = 10000;
+let popOfInvaders = 0;
+let isGameOver = false;
+let isOnPause = false;
+let isOnResume = true;
 
-
-// arrays containing the group for choices
 const easyGroups = [];
 const moderateGroups = [];
 const difficultGroups = [];
@@ -20,10 +25,11 @@ const difficulties = [1,2,3];
 // population
 let popOfHumansElement = document.querySelector('#pop-of-humans');
 let popOfInvadersElement = document.querySelector('#pop-of-invaders');
-
-//population variables
-let popOfHumans = Number(popOfHumansElement.innerText);
-let popOfInvaders = Number(popOfInvadersElement.innerText);
+popOfHumansElement.textContent = popOfHumans;
+popOfInvadersElement.textContent = popOfInvaders;
+// //population variables
+// let popOfHumans = Number(popOfHumansElement.innerText);
+// let popOfInvaders = Number(popOfInvadersElement.innerText);
 
 // capsules
 let capsuleContainer = document.querySelector('.capsule-container');
@@ -38,7 +44,14 @@ let choicesDisplayedD = document.querySelector('#choice4');
 // controls
 let startBtn = document.getElementById('start-btn');
 
-// EVENT LISTENERS
+//pre-play controls
+let prePlayOptions = document.querySelector('.pre-play-options');
+let prePlayPlayButton = document.getElementById('pre-play-play-btn');
+
+/*================================================================
+|                 EVENT LISTENERS                                 |
+/================================================================*/
+// callbacks
 function disableButton(btn) {
   btn.disabled = true;
 }
@@ -49,205 +62,97 @@ function enableButton(btn) {
 
 // adding of event listeners
 
-startBtn.addEventListener('click',startGame);
+startBtn.addEventListener('click', startGame);
+prePlayPlayButton.addEventListener('click', playGame)
+/*================================================================
+|                 MAIN FUNCTIONS                                  |
+/================================================================*/
 
-// even listener for choices buttons
-let getSelected = (e) => {
-
-  e.stopPropagation();
-
-  if(e.target.innerText != correctAnswer){
-    score = score <= 100? 0 : score - 100;
-    popOfHumans = popOfHumans <= 3000? 0 : popOfHumans - 3000;
-    popOfInvaders = popOfHumans <= 3000? popOfInvaders + popOfHumans : popOfInvaders + 3000;
-  } else {
-    score += 100;
-  }
-
-  choices.forEach((e) => disableButton(e));
-
-  updatePopulation();
-  updateScore();
-  
-  getNextCycle();  
+function playGame() {
+  prePlayOptions.classList.add('hide');
 };
 
-// HELPER FUNCTIONS
-// updates the population number and bar
-function updatePopulation() {
-  if(popOfHumans <= 0 ){
-    displayGameOver();
-  } else{
-    // popOfHumans = popOfHumans;
-    // popOfInvaders = popOfInvaders;
-    popOfHumansElement.textContent = popOfHumans;
-    popOfInvadersElement.textContent = popOfInvaders;
-  }
-
-  //updating the population bar
-  let percentOfHumans = popOfHumans/(popOfHumans + popOfInvaders);
-  let bar = document.getElementById('population-bar');
-  let barWidth = bar.offsetWidth;
-  let pxForHumans = Math.floor(percentOfHumans * barWidth);
-  let string = `grid-template-columns: ${pxForHumans}px 1fr`;
-  bar.style.cssText = string;
-  console.log(`popOfHumans/popOfInvaders ${popOfHumans} / ${popOfInvaders}`);
-}
-
-// updates the score 
-function updateScore(){
-  // console.log(`score: ${score}`);
-  document.getElementById('score-value').textContent = score;
-}
-
-// starts the next cycle after clicking one option 
-function getNextCycle(){
-  capsuleContainer.classList.remove('moving-down');
-  capsuleContainer.classList.add('at-bottom');
-  void capsuleContainer.offsetWidth;
-  setTimeout(function(){
-    capsuleContainer.classList.remove('at-bottom');
-    capsuleContainer.classList.add('moving-down');
-    clearInterval(cycle);
-    cycle = setInterval(sendGroup, cycleTime);
-    sendGroup();
-  }, 1000);
-}
-
-// gets a group from the database
-function getAGroup(fromDifficulties){
-  switch (fromDifficulties) {
-    case 1: {
-      return easyGroups[Math.floor(Math.random() * easyGroups.length)];
-    }
-    case 2: {
-      return moderateGroups[Math.floor(Math.random() * moderateGroups.length)];
-    }
-    case 3: {
-      return difficultGroups[Math.floor(Math.random() * difficultGroups.length)];
-    }
-    default: {
-      return easyGroups[0];
-    }
-  }  
-};
-
-//sending the selected group to be displayed on choices buttons
-function sendGroup(){
-  console.log(`--- STARTING A NEW CYCLE ---`);
-
-  choices.forEach((e) => enableButton(e));
-
-  let fromDifficulties = Math.ceil(Math.random() * difficulty);
-  // console.log(`fromDifficulties: ${fromDifficulties}`);
-  let currentGroup = getAGroup(fromDifficulties);
-  console.log(`currentGroup: [${currentGroup['a']}][${currentGroup['b']}][${currentGroup['c']}][${currentGroup['d']}][${currentGroup['key']}]`);
-
-  correctAnswer = currentGroup['key'];
-
-  // thinking to convert the following assignments to forEach for lesser lines of code but might affect the other calls to the elements in the displayed page plus the additional complexity of looping through the properties of the object to be assigned....so decided not to, violating DRY principle but i think it easier to understand
-  choicesDisplayedA.innerHTML = currentGroup['a'];
-  choicesDisplayedB.innerHTML = currentGroup['b'];
-  choicesDisplayedC.innerHTML = currentGroup['c'];
-  choicesDisplayedD.innerHTML = currentGroup['d'];
-
-  console.log(`correctAnswer: [${correctAnswer}]`);
-}
-
-// applies penalty if no choice was selected in a round
-function applyPenalty(){
-  console.log(`--- applying penalty---`);
-  capsuleContainer.classList.remove('moving-down');
-  void capsuleContainer.offsetWidth;
-  capsuleContainer.classList.add('moving-down');
-  if(!hasClicked){
-
-    score = score <= 100? 0 : score - 100;
-    popOfHumans = popOfHumans <= 3000? 0 : popOfHumans - 3000;
-    popOfInvaders = popOfHumans <= 3000? popOfInvaders + popOfHumans : popOfInvaders + 3000;
-
-    updatePopulation();
-    updateScore();
-  };
-}
-
-// ends the game
-function displayGameOver(){
-  alert('Game Over');
-}
-
-// loads the necessary things needed to run the game
-function loadGameElements(){
+function renderInitialDisplay(){
   document.querySelector('.game-container').classList.remove('hide');
-  choices.forEach((e) => e.addEventListener('click', getSelected));
-  document.querySelector('.capsule-container').classList.add('moving-down');
-  sendGroup();
-  // adding the animation of capsules
-  let element = document.querySelector('.moving-down');
-  element.addEventListener('animationend', applyPenalty);
-}
+  
+  //creates a loading indicator
+  let loading = document.createElement('div')
+  loading.textContent = 'Loading Game...';
+  loading.style.cssText = 'font-size: 2rem; position: absolute; top: 40%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; text-align: center;';
+  document.querySelector('main').appendChild(loading);
+
+  setTimeout(function(){
+    console.log(`loading game...`);
+    loading.classList.add('hide');
+    document.querySelector('.pre-play-options').classList.remove('hide');
+  },3000);
+  
+};
 
 //builds and populate the data needed to run the game
 //thinking of dividing this large function but since all these will be called
 //just a single time and only on the creation of the game data, decided in the end
 //not to break it down into smaller functions
-function loadGameData(){
-
-// function for creating the object containing the options for a group and the key for the correct answer
-// - will create the object regardless of the level of the group
-function createGroup(a, b, c, d, key){
-  this.a = a; this.b = b; this.c = c; this.d = d; this.key = key;
-}
-
-// function for creating the groups and sorts them to levels
-function addToGroups(a = 1, b = 0, c = 0, d = 0, key = 1, level = 1){
-  switch(level){
-    case 1: {
-      easyGroups.push(new createGroup(a, b, c, d, key)); break;
-    }
-    case 2: {
-      moderateGroups.push(new createGroup(a, b, c, d, key)); break;
-    }
-    case 3: {
-      difficultGroups.push(new createGroup(a, b, c, d, key)); break;
-    }
+function loadGameData() {
+  // function for creating the object containing the options for a group and the key for the correct answer
+  // - will create the object regardless of the level of the group
+  function createGroup(a, b, c, d, key){
+    this.a = a; this.b = b; this.c = c; this.d = d; this.key = key;
   }
+
+  // function for creating the groups and sorts them to levels
+  function addToGroups(a = 1, b = 0, c = 0, d = 0, key = 1, level = 1){
+    switch(level){
+      case 1: {
+        easyGroups.push(new createGroup(a, b, c, d, key)); break;
+      }
+      case 2: {
+        moderateGroups.push(new createGroup(a, b, c, d, key)); break;
+      }
+      case 3: {
+        difficultGroups.push(new createGroup(a, b, c, d, key)); break;
+      }
+    }
+  };
+
+  //populating the groups
+    // easy groups
+  addToGroups(14, 82, 70, 'AG', 'AG', 1);
+  addToGroups('🐳', '🐪', '🦓', '🐄', '🐳', 1);
+  addToGroups(14, 78, 13, 32, 13, 1);
+  addToGroups('toy', 'sea', 'gear', 'bar', 'gear', 1);
+  addToGroups('👄', '👀', '👂', '🔧', '🔧', 1);
+  addToGroups('🚲', '🚕', '🚌', '🚒', '🚲', 1);
+  addToGroups('🚲', '✈️', '🚌', '🏍', '✈️', 1);
+
+    // moderate groups
+  addToGroups('49', '36', '55', '25', '55', 2);
+  addToGroups('🌍', '🏀', '🚗', '💍', '🚗', 2);
+  addToGroups('🧤', '💼', '🩲', '👖', '💼', 2);
+  addToGroups('24', '15', '11', '48', '11', 2);
+  addToGroups('gin', 'ego', 'owl', 'boy', 'gin', 2);
+  addToGroups('kg', 'lb', 'oz', 'cm', 'cm', 2);
+
+    // difficult groups
+  addToGroups('10', '45', '21', '32', '32', 3);
+  addToGroups('3', '6', '28', '34', '34', 3);
+  addToGroups('Si', 'C', 'Ge', 'Au', 'Au', 3);
+  addToGroups('Fe', 'Cl', 'Ag', 'Au', 'Cl', 3);
+  addToGroups('🐍', '🐠', '🐄', '🐓', '🐄', 3);
 };
 
-//populating the groups
-  // easy groups
-addToGroups(14, 82, 70, 'AG', 'AG', 1);
-addToGroups('🐳', '🐪', '🦓', '🐄', '🐳', 1);
-addToGroups(14, 78, 13, 32, 13, 1);
-addToGroups('toy', 'sea', 'gear', 'bar', 'gear', 1);
-addToGroups('👄', '👀', '👂', '🔧', '🔧', 1);
-addToGroups('🚲', '🚕', '🚌', '🚒', '🚲', 1);
-addToGroups('🚲', '✈️', '🚌', '🏍', '✈️', 1);
-
-  // moderate groups
-addToGroups('49', '36', '55', '25', '55', 2);
-addToGroups('🌍', '🏀', '🚗', '💍', '🚗', 2);
-addToGroups('🧤', '💼', '🩲', '👖', '💼', 2);
-addToGroups('24', '15', '11', '48', '11', 2);
-addToGroups('gin', 'ego', 'owl', 'boy', 'gin', 2);
-addToGroups('kg', 'lb', 'oz', 'cm', 'cm', 2);
-
-  // difficult groups
-addToGroups('10', '45', '21', '32', '32', 3);
-addToGroups('3', '6', '28', '34', '34', 3);
-addToGroups('Si', 'C', 'Ge', 'Au', 'Au', 3);
-addToGroups('Fe', 'Cl', 'Ag', 'Au', 'Cl', 3);
-addToGroups('🐍', '🐠', '🐄', '🐓', '🐄', 3);
-
-}
-
-/*==================================================/
-|          FUNCTION TO START THE GAME               |
-/==================================================*/
 function startGame(){
   loadGameData();
-  loadGameElements();
-  cycle = setInterval(sendGroup, cycleTime);
+  renderInitialDisplay();
+  // playGame();// to remove if nailagay na sa button
+};
+// TODO
+function runIntro(){
+  // startGame(); // to remove kapag nalagay na sa button
+};
+
+function run(){
+  runIntro();
 }
 
-
+run();
---
+++ p2-js-game/assets/js/script2.js
@@ -0,0 +1,255 @@
+// GLOBAL VARIABLES
+
+let difficulty = 3;
+let correctAnswer;
+let score = 0;
+let cycleTime = 7000;
+let hasClicked = false;
+let cycle;
+let popOfHumans = 10000;
+let popOfInvaders = 0;
+let isGameOver = false;
+
+// arrays containing the group for choices
+const easyGroups = [];
+const moderateGroups = [];
+const difficultGroups = [];
+const difficulties = [1,2,3];
+
+
+
+// even listener for choices buttons
+let getSelected = (e) => {
+
+  e.stopPropagation();
+  hasClicked = true;
+  if(e.target.innerText != correctAnswer){
+    score = score <= 100? 0 : score - 100;
+    popOfInvaders = popOfHumans <= 3000? popOfInvaders + popOfHumans : popOfInvaders + 3000;
+    popOfHumans = popOfHumans <= 3000? 0 : popOfHumans - 3000;
+  } else {
+    score += 100;
+  }
+
+  choices.forEach((e) => disableButton(e));
+
+  updatePopulation();
+  updateScore();
+  checkForGameOver(popOfHumans);
+  
+  getNextCycle();  
+};
+
+// HELPER FUNCTIONS
+// updates the population number and bar
+function updatePopulation() {
+  
+  // popOfHumans = popOfHumans;
+  // popOfInvaders = popOfInvaders;
+  popOfHumansElement.textContent = popOfHumans;
+  popOfInvadersElement.textContent = popOfInvaders;
+
+  //updating the population bar
+  let percentOfHumans = popOfHumans/(popOfHumans + popOfInvaders);
+  let bar = document.getElementById('population-bar');
+  let barWidth = bar.offsetWidth;
+  let pxForHumans = Math.floor(percentOfHumans * barWidth);
+  let string = `grid-template-columns: ${pxForHumans}px 1fr`;
+  bar.style.cssText = string;
+  console.log(`popOfHumans/popOfInvaders ${popOfHumans} / ${popOfInvaders}`);
+}
+
+// updates the score 
+function updateScore(){
+  // console.log(`score: ${score}`);
+  document.getElementById('score-value').textContent = score;
+}
+
+// starts the next cycle after clicking one option 
+function getNextCycle(){
+  capsuleContainer.classList.remove('moving-down');
+  capsuleContainer.classList.add('at-bottom');
+  void capsuleContainer.offsetWidth;
+  setTimeout(function(){
+    capsuleContainer.classList.remove('at-bottom');
+    capsuleContainer.classList.add('moving-down');
+    clearInterval(cycle);
+    cycle = setInterval(runGame, cycleTime);
+    sendGroup();
+  }, 1000);
+  hasClicked = false;
+}
+
+// gets a group from the database
+function getAGroup(fromDifficulties){
+  switch (fromDifficulties) {
+    case 1: {
+      return easyGroups[Math.floor(Math.random() * easyGroups.length)];
+    }
+    case 2: {
+      return moderateGroups[Math.floor(Math.random() * moderateGroups.length)];
+    }
+    case 3: {
+      return difficultGroups[Math.floor(Math.random() * difficultGroups.length)];
+    }
+    default: {
+      return easyGroups[0];
+    }
+  }  
+};
+
+//sending the selected group to be displayed on choices buttons
+function sendGroup(){
+
+  console.log(`--- STARTING A NEW CYCLE ---`);
+
+  choices.forEach((e) => enableButton(e));
+
+  let fromDifficulties = Math.ceil(Math.random() * difficulty);
+  // console.log(`fromDifficulties: ${fromDifficulties}`);
+  let currentGroup = getAGroup(fromDifficulties);
+  console.log(`currentGroup: [${currentGroup['a']}][${currentGroup['b']}][${currentGroup['c']}][${currentGroup['d']}][${currentGroup['key']}]`);
+
+  correctAnswer = currentGroup['key'];
+
+  // thinking to convert the following assignments to forEach for lesser lines of code but might affect the other calls to the elements in the displayed page plus the additional complexity of looping through the properties of the object to be assigned....so decided not to, violating DRY principle but i think it easier to understand
+  choicesDisplayedA.innerHTML = currentGroup['a'];
+  choicesDisplayedB.innerHTML = currentGroup['b'];
+  choicesDisplayedC.innerHTML = currentGroup['c'];
+  choicesDisplayedD.innerHTML = currentGroup['d'];
+
+  console.log(`correctAnswer: [${correctAnswer}]`);
+  
+}
+
+// applies penalty if no choice was selected in a round
+function applyPenalty(){
+  console.log(`--- applying penalty---`);
+  capsuleContainer.classList.remove('moving-down');
+  void capsuleContainer.offsetWidth;
+  capsuleContainer.classList.add('moving-down');
+  if(!hasClicked){
+
+    score = score <= 100? 0 : score - 100;
+    popOfInvaders = popOfHumans <= 3000? popOfInvaders + popOfHumans : popOfInvaders + 3000;
+    popOfHumans = popOfHumans <= 3000? 0 : popOfHumans - 3000;
+
+    updatePopulation();
+    updateScore();
+  };
+}
+
+// checks game status for pause, game over
+function checkGameStatus(){
+  if (isGameOver){
+    // disabling choices and movement at game over
+    choices.forEach((e) => disableButton(e));
+    capsuleContainer.classList.remove('moving-down');
+    displayGameOver();
+  }
+}
+// checks for gameover
+function checkForGameOver(){
+  if(popOfHumans <= 0 ){
+    isGameOver = true;
+  }
+}
+// ends the game
+function displayGameOver(){
+  let gameStatus = document.createElement('div');
+  gameStatus.innerText = 'Game Over';
+  document.querySelector('#body').appendChild(gameStatus);
+  
+  
+  // alert('Game Over');
+
+}
+
+// loads the necessary things needed to run the game
+function loadGameElements(){
+  document.querySelector('.game-container').classList.remove('hide');
+  choices.forEach((e) => e.addEventListener('click', getSelected));
+  document.querySelector('.capsule-container').classList.add('moving-down');
+  sendGroup();
+  // adding the animation of capsules
+  let element = document.querySelector('.moving-down');
+  element.addEventListener('animationend', applyPenalty);
+
+  updatePopulation();
+  updateScore();
+}
+
+//builds and populate the data needed to run the game
+//thinking of dividing this large function but since all these will be called
+//just a single time and only on the creation of the game data, decided in the end
+//not to break it down into smaller functions
+function loadGameData(){
+
+// function for creating the object containing the options for a group and the key for the correct answer
+// - will create the object regardless of the level of the group
+function createGroup(a, b, c, d, key){
+  this.a = a; this.b = b; this.c = c; this.d = d; this.key = key;
+}
+
+// function for creating the groups and sorts them to levels
+function addToGroups(a = 1, b = 0, c = 0, d = 0, key = 1, level = 1){
+  switch(level){
+    case 1: {
+      easyGroups.push(new createGroup(a, b, c, d, key)); break;
+    }
+    case 2: {
+      moderateGroups.push(new createGroup(a, b, c, d, key)); break;
+    }
+    case 3: {
+      difficultGroups.push(new createGroup(a, b, c, d, key)); break;
+    }
+  }
+};
+
+//populating the groups
+  // easy groups
+addToGroups(14, 82, 70, 'AG', 'AG', 1);
+addToGroups('🐳', '🐪', '🦓', '🐄', '🐳', 1);
+addToGroups(14, 78, 13, 32, 13, 1);
+addToGroups('toy', 'sea', 'gear', 'bar', 'gear', 1);
+addToGroups('👄', '👀', '👂', '🔧', '🔧', 1);
+addToGroups('🚲', '🚕', '🚌', '🚒', '🚲', 1);
+addToGroups('🚲', '✈️', '🚌', '🏍', '✈️', 1);
+
+  // moderate groups
+addToGroups('49', '36', '55', '25', '55', 2);
+addToGroups('🌍', '🏀', '🚗', '💍', '🚗', 2);
+addToGroups('🧤', '💼', '🩲', '👖', '💼', 2);
+addToGroups('24', '15', '11', '48', '11', 2);
+addToGroups('gin', 'ego', 'owl', 'boy', 'gin', 2);
+addToGroups('kg', 'lb', 'oz', 'cm', 'cm', 2);
+
+  // difficult groups
+addToGroups('10', '45', '21', '32', '32', 3);
+addToGroups('3', '6', '28', '34', '34', 3);
+addToGroups('Si', 'C', 'Ge', 'Au', 'Au', 3);
+addToGroups('Fe', 'Cl', 'Ag', 'Au', 'Cl', 3);
+addToGroups('🐍', '🐠', '🐄', '🐓', '🐄', 3);
+
+}
+
+function restartGame(){
+
+}
+
+function runGame(){
+  checkForGameOver();
+
+  checkGameStatus();
+}
+/*==================================================/
+|          FUNCTION TO START THE GAME               |
+/==================================================*/
+
+function startGame(){
+  loadGameData();
+  loadGameElements();
+  cycle = setInterval(runGame, cycleTime);
+}
+
+
