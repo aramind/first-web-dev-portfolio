16802fa62376f23dcf952df22801845de421cbb7
aramind <r.m.miranda.ece@gmail.com>
2023-03-02 00:42:48 +0800

    feat: setting the elements of TitlePage.js

--- p3-react-app/src/pages/TitlePage.js
+++ p3-react-app/src/pages/TitlePage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const TitlePage = () => {
-  return <div>TitlePage</div>;
+  return (
+    <div className="page page--title">
+      <h1 className="title">MONitime</h1>
+      <p className="title-subtext">
+        <i>Track your time and optimize your life!</i>
+      </p>
+      <Link to="/monitimeapp">
+        <button className="btn btn--main-action">START</button>
+      </Link>
+      <small>
+        Powered by Gcash Donations. <NavLink>Donate Now.</NavLink>
+      </small>
+    </div>
+  );
 };
 
 export default TitlePage;
