03106a25c3dd1348bd77b991ce82f4fcb0973634
aramind <r.m.miranda.ece@gmail.com>
2023-03-02 21:44:13 +0800

    feat: creating the drowdown menu component in the add page

--- p3-react-app/src/pages/AddPage.js
+++ p3-react-app/src/pages/AddPage.js
@@ -1,7 +1,31 @@
 import React from "react";
+import DropdownList from "../components/DropdownList";
 
 const AddPage = () => {
-  return <div>AddPage</div>;
+  const options = [
+    { value: "sleep", label: "sleep" },
+    { value: "work", label: "work" },
+    { value: "learn", label: "learn" },
+    { value: "self", label: "self" },
+    { value: "social", label: "social" },
+    { value: "play", label: "play" },
+    { value: "h&fitness", label: "h&fitness" },
+    { value: "others", label: "others" },
+  ];
+
+  const handleSelect = (value) => {
+    console.log(`You selected ${value.value}`);
+  };
+
+  return (
+    <div>
+      <DropdownList
+        placeHolder="Select..."
+        options={options}
+        onChange={(value) => handleSelect(value)}
+      />
+    </div>
+  );
 };
 
 export default AddPage;
