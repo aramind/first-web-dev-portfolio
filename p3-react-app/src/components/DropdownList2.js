215d4d62b3c2070d1d46fc4224a5602aeb358f2c
aramind <r.m.miranda.ece@gmail.com>
2023-03-04 09:37:46 +0800

    feat: adding a confirmation prompt for the clear button

---
+++ p3-react-app/src/components/DropdownList2.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import "./DropdownList.css";
+
+const DropdownList = ({ placeHolder, options, onChange }) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  useEffect(() => {
+    const handler = () => setShowMenu(false);
+
+    window.addEventListener("click", handler);
+
+    return () => {
+      window.removeEventListener("click", handler);
+    };
+  });
+
+  const handleInputClick = (e) => {
+    e.stopPropagation();
+    setShowMenu(!showMenu);
+  };
+
+  const getDisplay = () => {
+    if (selectedValue) {
+      return selectedValue.label;
+    }
+    return placeHolder;
+  };
+
+  // handles the selection from the dropdown list
+  const onItemClick = (option) => {
+    setSelectedValue(option);
+    setShowMenu(false);
+    onChange(option);
+  };
+
+  const isSelected = (option) => {
+    if (!selectedValue) {
+      return false;
+    }
+
+    return selectedValue.value === option.value;
+  };
+
+  return (
+    <div className="dropdown-container">
+      <div
+        className="dropdown-input"
+        onClick={handleInputClick}
+      >
+        <div className="dropdown-selected-value">{getDisplay()}</div>
+        <div className="dropdown-tools">
+          <div className="dropdown-tool">V</div>
+        </div>
+      </div>
+      {showMenu && (
+        <div className="dropdown-menu">
+          {options.map((option, index) => (
+            <div
+              onClick={() => onItemClick(option)}
+              className={`dropdown-item ${isSelected(option) && "selected"}`}
+              key={`${option.value}-${index}`}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropdownList;
--- p3-react-app/src/pages/AddPage.js
+++ p3-react-app/src/pages/AddPage.js
@@ -34,18 +34,61 @@ const initialOptions = [
   { value: "fitness", label: "fitness" },
   { value: "others", label: "others" },
 ];
+//start of refactoring
+const initialState = {
+  todaysRecord:
+    JSON.parse(localStorage.getItem("records")) || initialTodaysRecord,
+  options: initialOptions,
+  activity: "others",
+  hours: 0,
+  minutes: 0,
+  hasError: false,
+  errorMsg: " ",
+};
 
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "SET_TODAYS_RECORD":
+      return { ...state, todaysRecord: payload.value };
+    case "SET_ACTIVITY":
+      return { ...state, activity: payload.value };
+    case "SET_HOURS":
+      return { ...state, hours: payload.value };
+    case "SET_MINUTES":
+      return { ...state, minutes: payload.value };
+    case "SET_HASERROR":
+      return { ...state, hasError: payload.value };
+    case "SET_ERROR_MSG":
+      return { ...state, errorMsg: payload.value };
+    case "RESET_FIELDS":
+      return {
+        ...state,
+        // todaysRecord: initialTodaysRecord,
+        // activity: "",
+        hours: 0,
+        minutes: 0,
+        hasError: false,
+        errorMsg: "",
+      };
+    default:
+      return state;
+  }
+};
+// end of refactoring
 const AddPage = () => {
-  // const [todaysRecord, setTodaysRecord] = useState(initialTodaysRecord);
-  const [todaysRecord, setTodaysRecord] = useState(
-    JSON.parse(localStorage.getItem("records")) || initialTodaysRecord
-  );
-  const [options, setOptions] = useState(initialOptions);
-  const [activity, setActivity] = useState("");
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hasError, setHasError] = useState(false);
-  const [errorMsg, setErrorMsg] = useState(" ");
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [showCleared, setShowCleared] = useState(false);
+
+  console.log(`rendering...`);
+  console.log(state.todaysRecord);
+  //start of refactoring
+
+  useEffect(() => {
+    localStorage.setItem("records", JSON.stringify(state.todaysRecord));
+  }, [state.todaysRecord, state.activity, state.hours, state.minutes]);
+
+  console.log(`rendering after...`);
+  console.log(state.todaysRecord);
 
   const labels = [
     "sleep",
@@ -58,12 +101,17 @@ const AddPage = () => {
     "others",
   ];
 
-  const updateRecord = () => {
-    localStorage.setItem("records", JSON.stringify(todaysRecord));
-    setMinutes(0);
-    setHours(0);
-    setActivity("");
-  };
+  let totalHrsRemaining =
+    24 -
+    labels.reduce((total, activity) => total + state.todaysRecord[activity], 0);
+
+  const tds = [];
+
+  for (const label of labels) {
+    const hrs = (+state.todaysRecord[label]).toFixed(1);
+    const percent = ((state.todaysRecord[label] / 24) * 100).toFixed(2);
+    tds.push({ label, hrs, percent });
+  }
 
   const updateTodaysRecord = (prevState, activity, hours, minutes) => {
     return {
@@ -72,100 +120,133 @@ const AddPage = () => {
     };
   };
 
-  const handleAdd = () => {
-    setTodaysRecord((prevState) =>
-      updateTodaysRecord(prevState, activity, hours, minutes)
-    );
-    updateRecord();
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // validations
+    if (
+      !state.activity.trim() ||
+      state.activity === "" ||
+      state.activity === null
+    ) {
+      console.log("error: no activity");
+      dispatch({ type: "SET_HASERROR", payload: { value: true } });
+      dispatch({
+        type: "SET_ERROR_MSG",
+        payload: { value: "Activity cannot be blank" },
+      });
+      return;
+    }
+
+    if (state.hours === 0 && state.minutes === 0) {
+      console.log("error: value");
+      dispatch({ type: "SET_HASERROR", payload: { value: true } });
+      dispatch({
+        type: "SET_ERROR_MSG",
+        payload: { value: "Duration cannot be blank" },
+      });
+      return;
+    }
   };
 
-  const handleSubtract = () => {
-    setTodaysRecord((prevState) =>
-      updateTodaysRecord(prevState, activity, -hours, -minutes)
-    );
-    updateRecord();
+  const handleAdd = (e) => {
+    handleFormSubmit(e);
+    if (!state.hasError) {
+      dispatch({
+        type: "SET_TODAYS_RECORD",
+        payload: {
+          value: updateTodaysRecord(
+            state.todaysRecord,
+            state.activity,
+            state.hours,
+            state.minutes
+          ),
+        },
+      });
+      dispatch({ type: "RESET_FIELDS" });
+    }
   };
 
-  const resetState = () => {
-    setTodaysRecord(initialTodaysRecord);
-    setMinutes(0);
-    setHours(0);
-    setActivity("");
+  const handleSubtract = (e) => {
+    handleFormSubmit(e);
+    if (!state.hasError) {
+      dispatch({
+        type: "SET_TODAYS_RECORD",
+        payload: {
+          value: updateTodaysRecord(
+            state.todaysRecord,
+            state.activity,
+            -state.hours,
+            -state.minutes
+          ),
+        },
+      });
+      dispatch({ type: "RESET_FIELDS" });
+    }
   };
 
   const handleClear = () => {
-    localStorage.setItem("records", JSON.stringify(initialTodaysRecord));
-    resetState();
+    let ans = "";
+    while (ans !== "n" && ans !== "y") {
+      ans = prompt(
+        "Are you sure you want to clear records for the current day? <y/n>"
+      );
+      ans.toLowerCase();
+    }
+    if (ans === "y") {
+      dispatch({ type: "RESET_FIELDS" });
+      localStorage.removeItem("records");
+      // dispatch({type: "SET_TODAYS_RECORD", payload: {value: }})
+      dispatch({
+        type: "SET_TODAYS_RECORD",
+        payload: { value: initialTodaysRecord },
+      });
+      setShowCleared(true);
+    } else return;
   };
 
-  // const handleClear = () => {
-  //   localStorage.setItem("records", JSON.stringify(initialTodaysRecord));
-  //   setTodaysRecord(initialTodaysRecord);
-  //   setMinutes(0);
-  //   setHours(0);
-  //   setActivity("");
-  // };
+  const handleActivityChange = (value) => {
+    dispatch({ type: "SET_ACTIVITY", payload: { value: value.value } });
+  };
 
-  // const handleAdd = () => {
-  //   setTodaysRecord((prevState) => ({
-  //     ...prevState,
-  //     [activity]: prevState[activity] + Number(hours) + Number(minutes / 60),
-  //   }));
-  //   updateRecord();
-  // };
+  const handleHoursChange = (e) => {
+    dispatch({
+      type: "SET_HOURS",
+      payload: { value: parseInt(e.target.value) },
+    });
+  };
 
-  // const handleSubtract = () => {
-  //   setTodaysRecord((prevState) => ({
-  //     ...prevState,
-  //     [activity]: prevState[activity] - Number(hours) - Number(minutes / 60),
-  //   }));
-  //   updateRecord();
-  // };
-
-  // const handleClear = () => {
-  //   localStorage.setItem("records", JSON.stringify(initialTodaysRecord));
-  //   setTodaysRecord(initialTodaysRecord);
-  //   setMinutes(0);
-  //   setHours(0);
-  //   setActivity("");
-  // };
-
-  let totalHrsRemaining =
-    24 - labels.reduce((total, activity) => total + todaysRecord[activity], 0);
-
-  console.log(totalHrsRemaining);
-  console.log(typeof totalHrsRemaining);
-
-  const tds = [];
-
-  for (const label of labels) {
-    const hrs = (+todaysRecord[label]).toFixed(1);
-    const percent = ((todaysRecord[label] / 24) * 100).toFixed(2);
-    tds.push({ label, hrs, percent });
-  }
+  const handleMinuteChange = (e) => {
+    dispatch({
+      type: "SET_MINUTES",
+      payload: { value: parseInt(e.target.value) },
+    });
+  };
 
   return (
     <div className="page add-page">
       <div className="add-page__inputs">
         <DropdownList
           placeHolder="Select Activity . . ."
-          options={options}
-          onChange={(value) => setActivity(value.value)}
+          options={state.options}
+          // onChange={(value) => setActivity(value.value)}
+          onChange={handleActivityChange}
         />
 
         <AddPageInputForm
           className="add-page__input input--hr"
           max={24}
           label="hrs and"
-          value={hours}
-          onChange={(e) => setHours(e.target.value)}
+          value={state.hours}
+          // onChange={(e) => setHours(e.target.value)}
+          onChange={handleHoursChange}
         />
         <AddPageInputForm
           className="add-page__input input--min"
           max={60}
           label="mins"
-          value={minutes}
-          onChange={(e) => setMinutes(e.target.value)}
+          value={state.minutes}
+          onChange={handleMinuteChange}
         />
 
         <div className="add-page__buttons">
@@ -182,13 +263,13 @@ const AddPage = () => {
         </div>
       </div>
 
-      <ErrorMessage message={errorMsg} />
+      <ErrorMessage message={state.errorMsg} />
       <div className="add-page__visuals">
         <div
           id="add-page-chart2"
           className="add-page-visual"
         >
-          <BarChart todaysRecord={todaysRecord} />
+          <BarChart todaysRecord={state.todaysRecord} />
         </div>
 
         <div
@@ -216,7 +297,7 @@ const AddPage = () => {
           id="add-page-chart"
           className="add-page-visual"
         >
-          <PieChart todaysRecord={todaysRecord} />
+          <PieChart todaysRecord={state.todaysRecord} />
         </div>
       </div>
 
