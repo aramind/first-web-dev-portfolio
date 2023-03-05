af447c60ddbd9fd2138911a63c1a3181bc84699c
aramind <r.m.miranda.ece@gmail.com>
2023-03-05 22:35:07 +0800

    feat: adding functionality that enables viewing more charts at each click of the add chart button on the charts page

--- p3-react-app/src/pages/ChartsPage.js
+++ p3-react-app/src/pages/ChartsPage.js
@@ -13,9 +13,38 @@ import ChartDisplay from "../components/ChartDisplay";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const ChartsPage = () => {
+  const [numCharts, setNumCharts] = useState(1);
+  const [chartComponents, setChartComponents] = useState([<ChartDisplay />]);
+
+  const addChart = () => {
+    if (numCharts < 7) {
+      const newChartComponents = [...chartComponents, <ChartDisplay />];
+      setChartComponents(newChartComponents);
+      setNumCharts((prevNum) => prevNum + 1);
+    }
+  };
+
+  const removeChart = (indexToRemove) => {
+    const newChartComponents = chartComponents.filter(
+      (_, index) => index !== indexToRemove
+    );
+    setChartComponents(newChartComponents);
+    setNumCharts((prevNum) => prevNum - 1);
+  };
+
   return (
     <div className="page page--chart">
-      <ChartDisplay />
+      <div className="chart-container">
+        {chartComponents.map((chartComponent, index) => (
+          <div key={index}>
+            {chartComponent}
+            {numCharts > 1 && (
+              <button onClick={() => removeChart(index)}>Close</button>
+            )}
+          </div>
+        ))}
+        <button onClick={addChart}>Add Chart</button>
+      </div>
       {/* UPGRADE TO PREMIUM PAGE */}
       {/* <div className="message">
         <p>Charting Features are available exclusively to Premium Users.</p>
