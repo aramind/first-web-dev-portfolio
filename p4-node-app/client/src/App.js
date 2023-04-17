0eed760efbb0d9c1f1b654befdb9ed6fb846d0cc
aramind <r.m.miranda.ece@gmail.com>
2023-04-18 02:47:01 +0800

    feat: adding MUI dependencies

--- p4-node-app/client/src/App.js
+++ p4-node-app/client/src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>
+        The time monitoring app allows users to track their daily activities and
+        the time they spend on them, such as sleep, work, learning, errands, and
+        self-care. By analyzing and summarizing the data, the app provides
+        insights to help users become more productive and efficient by
+        identifying areas where they need to adjust their time allocation.
+        Additionally, the app features a selection of quotes related to
+        productivity, work-life-balance, and time management that users can view
+        to stay motivated and inspired.
+      </p>
+    </div>
+  );
 }
 
 export default App;
