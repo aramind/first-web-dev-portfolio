3ead45dc41227d7c462cd61c67ad6189f13449cf
aramind <r.m.miranda.ece@gmail.com>
2023-04-21 14:24:50 +0800

    feat: initial set up for the index.js file of the server

--- p4-node-app/server/index.js
+++ p4-node-app/server/index.js
@@ -1,1 +1,41 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Authorization"
+  );
+  next();
+});
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req, res) => res.json({ message: "Welcome to our API" }));
+app.use((req, res) =>
+  res.status(404).json({ success: false, message: "Not Found" })
+);
+
+const startServer = async () => {
+  try {
+    // connect to mongoDB
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
