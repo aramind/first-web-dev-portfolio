e60ef5fb1cb29a454c5dfad3c3583a1dc51d9bae
aramind <r.m.miranda.ece@gmail.com>
2023-04-22 15:09:43 +0800

    feat: adding the Record schema in models of server

---
+++ p4-node-app/server/models/Record.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const activitySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  hours_spent: { type: String, required: true },
+});
+
+const recordSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true,
+  },
+  last_modified: { type: Date, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  activities: [activitySchema],
+});
+
+module.exports = mongoose.model("Record", recordSchema);
