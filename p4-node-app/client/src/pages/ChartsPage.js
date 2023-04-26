24fab6066a5a2df6d1b2ba566c249abadda2d9a6
aramind <r.m.miranda.ece@gmail.com>
2023-04-26 10:54:00 +0800

    feat: implementing the add chart button on the charts pages
      - at the click of the button, a chart component will be added on the display

--- p4-node-app/client/src/pages/ChartsPage.js
+++ p4-node-app/client/src/pages/ChartsPage.js
@@ -1,9 +1,25 @@
-import React from "react";
-import { Box, Toolbar } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Stack, Toolbar, Typography } from "@mui/material";
 import ChartDisplayWithDetails from "../components/charts/ChartDisplayWithDetails";
+import { v4 as uuidv4 } from "uuid";
 import muiTheme from "../muiTheme";
 
+import chartImage from "../resources/images/img-pie-chart.png";
+
 const ChartsPage = () => {
+  // local states
+  const [charts, setCharts] = useState([]);
+  const addChart = () => {
+    setCharts([
+      ...charts,
+      { chart: <ChartDisplayWithDetails />, chartid: uuidv4() },
+    ]);
+  };
+
+  const removeChart = (id) => {
+    setCharts(charts.filter((chart) => chart.id !== id));
+  };
+
   return (
     <Box
       // alignItems={"center"}
@@ -20,8 +36,41 @@ const ChartsPage = () => {
       }}
     >
       <Toolbar sx={{ marginBottom: "10px" }} />
-      <Box>
-        <ChartDisplayWithDetails />
+      {/* MAIN CONTENT */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "2rem",
+        }}
+      >
+        <>
+          {charts.map((chartObj) => chartObj.chart)}
+          <Box
+            width="320px"
+            height="480px"
+            // borderRadius="10px" // sx={{ border: "1px solid red" }}
+          >
+            <Button
+              fullWidth
+              id="add-chart-button"
+              variant="outlined"
+              borderRadius="10px"
+              p={2}
+              sx={{ height: "100%" }}
+              onClick={addChart}
+            >
+              <Stack gap={2}>
+                <Typography>Add Chart</Typography>
+                <img
+                  src={chartImage}
+                  alt="pie chart"
+                ></img>
+              </Stack>
+            </Button>
+          </Box>
+        </>
       </Box>
     </Box>
   );
